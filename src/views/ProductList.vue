a4ee286b3a2ec572531aebfd7cf922aaca7ebe45
Leo Rodrei <jumarangleo93@gmail.com>
2024-04-19 21:11:22 +0800

    add productlist

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -34,6 +34,153 @@
           <router-link to="/add" class="nav-link">Add Product</router-link>
         </div>
       </div>
+      <ul>
+        <li
+          v-for="product in products"
+          :key="product.id"
+          class="flex justify-between items-start p-3 border-b border-gray-300"
+        >
+          <div :key="product.updatedAt">
+            <div class="flex text-2xl font-medium">
+              <h2 class="me-2">{{ product.name }}</h2>
+              <h2 class="text-gray-400">₱ {{ product.price }}</h2>
+            </div>
+
+            <div>
+              <h6 class="text-gray-800">{{ product.description }}</h6>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <form
+              @submit.prevent="saveChanges(product.id)"
+              class="product-form"
+            >
+              <BaseModal :visible="product.isEditModalOpen">
+                <div class="space-y-7 p-5">
+                  <div class="flex flex-col space-y-1">
+                    <h1 class="text-2xl font-semibold">Edit Product</h1>
+                    <h1 class="text-gray-400">
+                      Make changes to this product. Click 'Save' when you're
+                      done.
+                    </h1>
+                  </div>
+                  <div>
+                    <label
+                      for="name"
+                      class="block mb-2 text-sm font-medium text-black dark:text-white bg-"
+                      >Name</label
+                    >
+                    <input
+                      type="text"
+                      id="name"
+                      v-model="editedProductName"
+                      v-if="editingProduct.id === product.id"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring focus:ring-blue-500 block w-full p-2.5"
+                      placeholder="Surf Poweder e.g"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      for="description"
+                      class="block mb-2 text-sm font-medium text-black dark:text-white bg-"
+                      >Description</label
+                    >
+                    <textarea
+                      id="description"
+                      v-model="editedDescription"
+                      v-if="editingProduct.id === product.id"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring focus:ring-blue-500 block w-full p-2.5"
+                      required
+                      placeholder=" - Surf powder provides you with an excellent cleaning with fresh fragrances, giving your laundry a lift with every wash. e.g"
+                    ></textarea>
+                  </div>
+
+                  <div>
+                    <label
+                      for="price"
+                      class="block mb-2 text-sm font-medium text-black dark:text-white bg-"
+                      >Price (₱)</label
+                    >
+                    <input
+                      type="number"
+                      id="price"
+                      v-model.number="editedPrice"
+                      v-if="editingProduct.id === product.id"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring focus:ring-blue-500 block w-full p-2.5"
+                      required
+                    />
+                  </div>
+
+                  <div class="flex justify-end space-x-3">
+                    <button
+                      @click="cancelEditing"
+                      class="bg-white0 hover: font-normal py-2 px-4 rounded"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      class="bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </BaseModal>
+            </form>
+
+            <BaseModal :visible="isDeleteModalOpen">
+              <div class="flex flex-col justify-center space-y-8 p-5">
+                <div class="flex flex-col">
+                  <h6 class="text-2xl font-semibold">Delete</h6>
+                  <h6 class="text-gray-400">
+                    Click 'Yes' if you want to delete the product.
+                  </h6>
+                </div>
+                <div class="flex flex-col">
+                  <div>
+                    <h6 class="text-lg font-medium">
+                      Are you sure you want to delete this product?
+                    </h6>
+                  </div>
+
+                  <div class="flex justify-end space-x-5 mt-4">
+                    <button @click="closeDeleteModal">No</button>
+                    <button
+                      class="bg-red-500 hover:bg-red-600 text-white font-normal py-2 px-4 rounded"
+                      @click="deleteProduct()"
+                    >
+                      Yes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </BaseModal>
+            <div class="space-x-1 flex">
+              <button
+                v-if="!isEditModalOpen || editingProduct.id !== product.id"
+                @click="startEditing(product)"
+                class=""
+              >
+                <v-icon
+                  name="md-modeeditoutline-outlined"
+                  scale="2"
+                  class="text-gray-500 hover:bg-gray-200 rounded-md p-2"
+                ></v-icon>
+              </button>
+
+              <button class="" @click="openDeleteModal(product)">
+                <v-icon
+                  name="md-deleteoutline-outlined"
+                  scale="2"
+                  class="text-gray-500 hover:bg-gray-200 rounded-md p-2"
+                ></v-icon>
+              </button>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -120,26 +267,5 @@ export default {
 </script>
 
 <style scoped>
-.list-enter-active {
-  opacity: 0;
-  transform: translateY(100px);
-  transition: all 0.3s ease-in-out;
-}
-.list-leave-active {
-  opacity: 0;
-  transform: translateY(-100px);
-  transition: all 0.3s ease-in-out;
-}
-.slide-fade-enter-active {
-  transition: opacity 0.3s, transform 0.3s;
-}
-.slide-fade-leave-active {
-  transition: opacity 0.8s cubic-bezier(1, 0.5, 0.8, 1),
-    transform 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-enter,
-.slide-fade-leave-to {
-  opacity: 0;
-  transform: translateY(-20px);
-}
+
 </style>
